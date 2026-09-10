f020254b25498bf66d84e4cc737a752238191a15
Ponce David <fernandoponce@ulp.edu.ar>
2026-09-10 00:01:32 -0300

    feat(app): layout | menu lateral | tabs | header
    - Layout unico: menu, tabs y contenedor.
    - Header, menu, tabs y vidriera como componentes de estructura.

---
+++ RavEatApp/src/components/estructura/comp_tabs.vue
@@ -0,0 +1,37 @@
+<template>
+	<ion-tab-bar slot="bottom">
+		<ion-tab-button
+			v-for="item in items_tabs"
+			:key="item.id"
+			:tab="item.id"
+			:href="item.ruta"
+		>
+			<ion-icon :icon="item.icono" />
+			<ion-label>{{ item.titulo }}</ion-label>
+		</ion-tab-button>
+	</ion-tab-bar>
+</template>
+
+<script>import {
+	IonIcon,
+	IonLabel,
+	IonTabBar,
+	IonTabButton
+} from '@ionic/vue';
+import { obtener_tabs_rol } from '@/config/navegacion';
+
+export default {
+	name: 'comp_tabs',
+	components: {
+		IonIcon,
+		IonLabel,
+		IonTabBar,
+		IonTabButton
+	},
+	computed: {
+		items_tabs(){
+			// Sin rol activo devuelve todo, recortado al maximo de tabs. En v5 filtra por rol.
+			return obtener_tabs_rol(null);
+		}
+	}
+};</script>
---
+++ RavEatApp/src/components/estructura/comp_vidriera.vue
@@ -0,0 +1,22 @@
+<template>
+	<header class="raveat-vidriera">
+		<img class="raveat-vidriera__logo" src="/logo_final.png" alt="RavEat" />
+		<p v-if="lema" class="raveat-vidriera__lema">{{ lema }}</p>
+		<slot />
+	</header>
+</template>
+
+<script>
+// El encabezado de bienvenida de la pantalla de inicio. En v1 estaba escrito a mano dentro de
+// la vista; acá se extrae porque a partir de v3 lo usa tambien la vitrina, que le mete un
+// buscador por el slot. Los estilos viven en theme/global.css, no scoped: son el lenguaje
+// visual de la app y se comparten entre versiones.
+export default {
+	name: 'comp_vidriera',
+	props: {
+		lema: {
+			type: String,
+			default: ''
+		}
+	}
+};</script>
